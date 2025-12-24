2d201d4a1b4e1d292758e64aaba237f4dce1b0b4
Nehansh-alwala <alwalanehansh@gmail.com>
2025-12-24 11:51:54 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navabar from './components/Navabar.JSX'
+
+const App = () => {
+  return (
+    <main>
+      <Navabar />
+    </main>
+  )
+}
+
+export default App
